0990e4fc258c66a2cb0a6e1b0146d5e19efee1f4
nwj <nwj@PF3113G5>
2022-02-10 18:46:49 +0100

    Djikstra.net, might be an easier framework

---
+++ telstarapp/QuikGraph/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuikGraph;
+using QuikGraph.Algorithms;
+
+namespace telstarapp.QuikGraph
+{
+    public class Class1
+    {
+
+    }
+}
